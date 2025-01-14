7ee0878f907666bc97f074cdea765f4ced0e7f36
deepanjali-nk <DeepanjaliBScCSIT2077@Prime.edu.np>
2025-01-14 17:08:20 +0545

    chore: post request

--- part3/notes-server/index.js
+++ part3/notes-server/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express();
+app.use(express.json())
 
 console.log('Hello');
 
@@ -26,7 +27,7 @@ let notes = [
 //   response.end(JSON.stringify(notes));
 // })
 
-app.get("/api",((request,response)=>{
+app.get("/api/notes",((request,response)=>{
     response.json(notes);
 }))
 app.get("/api/notes/:id",((request,response)=>{
@@ -41,16 +42,19 @@ app.get("/api/notes/:id",((request,response)=>{
 }))
 app.delete("/api/notes/:id",((request,response)=>{
   const getId= (request.params.id);
-  const myNote=(notes.find((note)=> note.id===getId));
+  notes=(notes.filter((note)=> note.id!==getId));
 
-  if(myNote){
-    response.json(myNote);
-  }else{
-    response.status(404).send(`Theres no note at ${getId}`);
-  }
+  response.status(204).send(`The note at ${getId} has been deleted`)
 }))
 
+app.post("/api/notes",(request,response)=>{
+  const myNewPost= request.body;
+  myNewPost.id = notes.length+1;
+  notes.push(myNewPost);
+  console.log(myNewPost);
+  response.status(201).json(myNewPost)
 
+})
 const PORT = 3002;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`)
