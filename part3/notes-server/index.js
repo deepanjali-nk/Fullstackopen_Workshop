7884cc606a4bbffa4c8e35fed0a0777fb2d52794
deepanjali-nk <deepanjalinagarkoti@gmail.com>
2025-01-21 08:42:01 +0545

    part3 b workshop

--- part3/notes-server/index.js
+++ part3/notes-server/index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 app.use(express.json())
 
 console.log('Hello');
@@ -57,6 +60,6 @@ app.post("/api/notes", (request, response) => {
 });
 
 
-const PORT = 3002;
+const PORT = 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`)
